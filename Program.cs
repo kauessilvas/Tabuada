ba90729a50d42cf69d23066155cfaba53d2eefca
kauessilvas <kauesantosds5@gmail.com>
2025-05-24 19:35:35 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Clear();
+
+Console.WriteLine("======== Tabuada ========");
+
+int numero;
+
+while (true)
+{
+    Console.Write("\nDigite um número inteiro: ");
+    string entrada = Console.ReadLine()!;
+
+    bool sucesso = int.TryParse(entrada, out numero);
+
+    if (sucesso)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("\nEntrada inválida! Por favor, digite apenas números inteiros.");
+    }
+}
+
+string[] resultadoTabuada = calculaTabuada(numero);
+
+foreach (string linha in resultadoTabuada)
+{
+    Console.WriteLine(linha);
+}
+
+static string[] calculaTabuada(int numero)
+{
+    string[] tabuada = new string[11];
+    for (int i = 0; i <= 10; i++)
+    {
+        int resultado = numero * i;
+        tabuada[i] = $"{numero} x {i} = {resultado}";
+    }
+    return tabuada;
+}
